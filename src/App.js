6eecab6e8792856ae9cc08c402c50be949e17824
batel shriki <batel2610@gmail.com>
2022-08-21 18:22:09 +0300

    monsters first

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log("render");
+
+    const filterdMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={(e) => {
+            console.log(e.target.value);
+            const searchField = e.target.value.toLocaleLowerCase();
+            // [ { name: 'linda} {name: 'gil'}]
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        ></input>
+        {filterdMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
